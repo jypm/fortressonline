3b034a761ddf9ceabee77681925bd9df32329057
jypm <87839718+jypm@users.noreply.github.com>
2026-08-18 20:35:10 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'fortress-v1';
+const FILES = [
+  '/fortressonline/',
+  '/fortressonline/index.html',
+  '/fortressonline/manifest.json',
+  '/fortressonline/icon-192.png',
+  '/fortressonline/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
